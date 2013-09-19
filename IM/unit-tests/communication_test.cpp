e752af7900d3141209d1264e4a59a376a68cbf08
Franco Chiappori <franco.chiappori@bbv.ch>
2013-09-19 09:28:27 +0200

    Implemented and tested keep alive message

--- IM/unit-tests/communication_test.cpp
+++ IM/unit-tests/communication_test.cpp
@@ -6,6 +6,39 @@
 #include "mocks/qudpsocket_mock.h"
 #include "communication_test.h"
 
+void CommunicationTest::handle_send_keep_alive_broadcasts_the_message_over_udp()
+{
+    // arrange
+    qRegisterMetaType<QHostAddress>("QHostAddress");
+    QUdpSocketMock udp_socket;
+    QSignalSpy writeDatagram(&udp_socket, SIGNAL(called_writeDatagram(QByteArray const &, QHostAddress const &, quint16)));
+
+    // act
+    IM::Communication testee(udp_socket);
+    const QString expected_nickname = "Travis";
+    testee.handle_send_keep_alive(expected_nickname);
+
+    // assert
+    QCOMPARE(writeDatagram.count(), 1);
+
+    const auto arguments = writeDatagram.takeFirst();
+    QDataStream data(arguments.at(0).toByteArray());
+
+    quint32 command;
+    QString nickname;
+    data >> command >> nickname;
+
+    const quint32 expected_command = IM::Command::KeepAlive;
+    QCOMPARE(command, expected_command);
+    QCOMPARE(nickname, expected_nickname);
+
+    const QHostAddress expected_address = QHostAddress::Broadcast;
+    QCOMPARE(qvariant_cast<QHostAddress>(arguments.at(1)), expected_address);
+
+    const quint32 expected_port = 41000;
+    QCOMPARE(arguments.at(2).toUInt(), expected_port);
+}
+
 void CommunicationTest::handle_send_message_broadcasts_the_message_over_udp()
 {
     // arrange
@@ -40,7 +73,5 @@ void CommunicationTest::handle_send_message_broadcasts_the_message_over_udp()
 
     const quint32 expected_port = 41000;
     QCOMPARE(arguments.at(2).toUInt(), expected_port);
-
-
 }
 
